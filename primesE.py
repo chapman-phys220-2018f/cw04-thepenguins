fcf89dc1d433d92a9474fa04e6781e903207da65
Enea Dodi <dodi@chapman.edu>
2018-09-26 19:24:18 +0000

    a working primes!

--- primesE.py
+++ primesE.py
@@ -7,12 +7,12 @@
 # Course: PHYS220/MATH220/CPSC220 Fall 2018 Assignment: CW03
 ###
 
-'''Eratosthanes module
-This module contains the eratosthanes function that generates
+'''Eratosthenes module
+This module contains the eratosthenes function that generates
 primes up to the value of positive integer input n'''
 import math
-def eratosthanes(n):
-    '''Erastosthanes function
+def eratosthenes(n):
+    '''Erastosthenes function
     Args: n - positive integer representing ceiling number
     Returns: list of all primes less than n'''
     
@@ -25,13 +25,25 @@ def eratosthanes(n):
     nums = nums[2:]
     
     #removes all even numbers
-    nums = [a for a in nums if a % 2 != 0]
+    nums = [a for a in nums if ((a % 2 != 0) or (a==2))]
+    
     for i in nums:
         if i > math.sqrt(n):
             break
-        for a in nums:
-            if (a%i == 0 and a>i):
-                nums.remove(a)
+        else:
+            for counter in nums:
+                if(counter!=i and counter%i==0):
+                    nums.remove(counter)
+            
     return nums
 
-def gen_eratosthanes
+def gen_eratosthenes():
+    nums = list(range(10000))
+    nums = nums[2:]
+    nums = [a for a in nums if ((a % 2 != 0) or (a==2))]
+    while True:
+        for i in nums:
+            for counter in nums:
+                if(counter==i and counter%i!=0):
+                    yield nums
+                nums.remove(counter)
